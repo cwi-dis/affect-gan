57482d1bc2fdf7131b6f2dd3218168bb29673f3f
andreifurdui <andreifurdui.g@gmail.com>
2020-02-18 13:32:55 +0100

    callback try #4

--- sources/util/callbacks.py
+++ sources/util/callbacks.py
@@ -1,5 +1,7 @@
 import tensorflow as tf
 from tensorflow.keras import callbacks
+from tensorflow.python.eager import context
+from tensorflow.python.ops import summary_ops_v2
 
 from datetime import datetime
 import os
@@ -15,6 +17,51 @@ class MetricsCallback(callbacks.TensorBoard):
         lr = self.model.optimizer.lr
         logs.update({'lr': lr})
         super().on_epoch_end(epoch, logs)
+
+    def _log_metrics(self, logs, prefix, step):
+        """Writes metrics out as custom scalar summaries.
+        Arguments:
+            logs: Dict. Keys are scalar summary names, values are NumPy scalars.
+            prefix: String. The prefix to apply to the scalar summary names.
+            step: Int. The global step to use for TensorBoard.
+        """
+        print(f"\n\n{logs}\n\n")
+        if logs is None:
+            logs = {}
+
+        # Group metrics by the name of their associated file writer. Values
+        # are lists of metrics, as (name, scalar_value) pairs.
+        logs_by_writer = {
+            self._train_run_name: [],
+            self._validation_run_name: [],
+        }
+        validation_prefix = 'val_'
+        for (name, value) in logs.items():
+            if name in ('batch', 'size', 'num_steps'):
+                # Scrub non-metric items.
+                continue
+            print(f"\n\n{name} : {value}\n\n")
+            if name.startswith(validation_prefix):
+                name = name[len(validation_prefix):]
+                writer_name = self._validation_run_name
+            else:
+                writer_name = self._train_run_name
+            print(f"\n\n{writer_name}\n\n")
+            name = prefix + name  # assign batch or epoch prefix
+            logs_by_writer[writer_name].append((name, value))
+
+        with context.eager_mode():
+            with summary_ops_v2.always_record_summaries():
+                for writer_name in logs_by_writer:
+                    these_logs = logs_by_writer[writer_name]
+                    if not these_logs:
+                        # Don't create a "validation" events file if we don't
+                        # actually have any validation data.
+                        continue
+                    writer = self._get_writer(writer_name)
+                    with writer.as_default():
+                        for (name, value) in these_logs:
+                            summary_ops_v2.scalar(name, value, step=step)
 
 
 class CallbacksProducer:
