07b5eba4d04f8d747451d85034d2ea487f7f19d2
andreifurdui <andreifurdui.g@gmail.com>
2020-05-11 18:29:28 +0300

    try #1

--- sources/data/datagenerator.py
+++ sources/data/datagenerator.py
@@ -18,8 +18,21 @@ class DataGenerator:
             gen_sig = self.generator(generator_inputs, training=False)
             yield gen_sig, generator_class_inputs
 
-def main():
+def init_tf_gpus():
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
 
+    if gpus:
+        try:
+            for gpu in gpus:
+                tf.config.experimental.set_memory_growth(gpu, True)
+            logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+            print("Physical GPUs available: %d  -- Logical GPUs available: %d" % (len(gpus), len(logical_gpus)))
+        except RuntimeError as e:
+            print(e)
+
+def main():
+    init_tf_gpus()
     datagenerator = tf.data.Dataset.from_generator(
         generator=DataGenerator("../Logs/wgan-gp-big/model_gen"),
         output_types=(tf.float32, tf.float32)
