2055f6143e7a13b2a5a04a66fdeaf93ee15a170c
andreifurdui <andreifurdui.g@gmail.com>
2020-06-05 22:26:52 +0300

    BaseNET final

--- sources/models/BaseNET2.py
+++ sources/models/BaseNET2.py
@@ -9,37 +9,37 @@ class BaseNET2(tf.keras.Model):
     def __init__(self, hparams):
         super(BaseNET2, self).__init__()
 
-        self.downres0 = DownResLayer(
-            channels_out=4,
-            dropout_rate=0.25,
-            kernel_size=6,
-            normalization="batch",
-            first_layer=True,
-            regularization=tf.keras.regularizers.l2(0.0005)
-        )
-
-        self.downres1 = DownResLayer(
-            channels_out=4*2,
-            dropout_rate=0.25,
-            kernel_size=6,
-            normalization="batch",
-            regularization=tf.keras.regularizers.l2(0.0005)
-        )
-
-        self.max0 = layers.MaxPool1D(
-            pool_size=2,
-            strides=2,
+        self.expand = layers.Conv1D(
+            filters=6,
+            kernel_size=5,
             padding="same"
         )
-        self.drop1 = layers.Dropout(0.5)
+
+        self.downres0 = DownResLayer(
+            channels_out=8,
+            dropout_rate=0.0,
+            kernel_size=6,
+            first_layer=False,
+            regularization=tf.keras.regularizers.l2(0.0005)
+        )
+
+        self.conv3 = layers.Conv1D(
+            filters=10,
+            kernel_size=25,
+            strides=5,
+            padding="same",
+            kernel_regularizer=tf.keras.regularizers.l2(0.0005)
+        )
+
         self.flat = layers.Flatten()
+        self.drop1 = layers.Dropout(0.5)
         self.dense_out = layers.Dense(2, activation="softmax")
 
     def call(self, inputs, training=None, mask=None):
 
-        x = self.downres0(inputs, training=training)
-        x = self.downres1(x, training=training)
-        x = self.max0(x)
+        x = self.expand(inputs)
+        x = self.downres0(x, training=training)
+        x = self.conv3(x)
 
         x = self.flat(x)
         x = self.drop1(x, training)
